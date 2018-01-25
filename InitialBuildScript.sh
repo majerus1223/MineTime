0ad3089d669a7119bfc6ae82269036a4515ebe66
majerus <majerus.one@gmail.com>
2018-01-25 10:35:42 -0600

    Adding Build Script Initial

---
+++ InitialBuildScript.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+add-apt-repository ppa:graphics-drivers/ppa -y 
+apt-get update
+sleep 5
+apt-get install htop screen nvidia-387 git openssh-server -y
+mkdir -p $HOME/Desktop/mining/downloads
+mkdir -p $HOME/Desktop/mining/scripts
+mkdir -p $HOME/Desktop/mining/other
+mkdir -p $HOME/Desktop/mining/miners
+
+service ssh start
+
+#Download Miner
+
+wget https://eggpool.net/dl/EggMinerGpuLin2.tgz?1516891778 -O $HOME/Desktop/mining/downloads/EggMiner.tgz.gz
+
+tar -xvzf $HOME/Desktop/mining/downloads/EggMiner.tgz.gz -C $HOME/Desktop/mining/miners
+
+git -C $HOME/Desktop/mining/scripts/ clone https://github.com/majerus1223/linux_gpu_mining
+
+#Set insane permissions
+chmod 777 -R $HOME/Desktop/mining 
+
+shutdown -r now
