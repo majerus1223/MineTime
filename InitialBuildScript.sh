c5bd4c960ed7cb4ad00d8b481bf392bed6193f2d
majerus <majerus.one@gmail.com>
2018-01-26 13:53:45 -0600

    Added team viewer and older nvidia driver

--- InitialBuildScript.sh
+++ InitialBuildScript.sh
@@ -3,8 +3,8 @@
 add-apt-repository ppa:graphics-drivers/ppa -y 
 apt-get update
 sleep 5
-#remove nvidia-387
-apt-get install htop screen git openssh-server -y
+
+apt-get install htop screen git nvidia-387 openssh-server -y
 mkdir -p $HOME/Desktop/mining/downloads
 mkdir -p $HOME/Desktop/mining/scripts
 mkdir -p $HOME/Desktop/mining/other
@@ -12,11 +12,20 @@ mkdir -p $HOME/Desktop/mining/miners
 
 service ssh start
 
-#Download Miner
+#Download Teamviewer & Miner
+
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb -O $HOME/Desktop/mining/download/teamviewer_amd64.deb
 
 wget https://eggpool.net/dl/EggMinerGpuLin2.tgz?1516891778 -O $HOME/Desktop/mining/downloads/EggMiner.tgz.gz
 
 tar -xvzf $HOME/Desktop/mining/downloads/EggMiner.tgz.gz -C $HOME/Desktop/mining/miners
+
+chmod 777 $HOME/Desktop/mining/download/teamviewer_amd64.deb
+
+#install teamviewer
+dpkg -i $HOME/Desktop/mining/download/teamviewer_amd64.deb
+#resolve dependencies
+apt-get -f install -y 
 
 git -C $HOME/Desktop/mining/scripts/ clone https://github.com/majerus1223/MineTime
 
