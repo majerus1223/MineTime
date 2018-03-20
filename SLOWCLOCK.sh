053175a94a35e62ad9052618c1949a0eee2be434
James Minemachine <ACJMINING@GMAIL.COM>
2018-03-19 20:29:59 -0500

    new clocks

--- SLOWCLOCK.sh
+++ SLOWCLOCK.sh
@@ -1,7 +1,3 @@
-#Running higher clock rates then before
-#Testing Version
-
-
 nvidia-settings -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed=85
 nvidia-settings -a [gpu:1]/GPUFanControlState=1 -a [fan:1]/GPUTargetFanSpeed=85
 nvidia-settings -a [gpu:2]/GPUFanControlState=1 -a [fan:2]/GPUTargetFanSpeed=85
@@ -15,13 +11,11 @@ nvidia-settings -a [gpu:9]/GPUFanControlState=1 -a [fan:9]/GPUTargetFanSpeed=85
 nvidia-settings -a [gpu:10]/GPUFanControlState=1 -a [fan:10]/GPUTargetFanSpeed=85
 nvidia-settings -a [gpu:11]/GPUFanControlState=1 -a [fan:11]/GPUTargetFanSpeed=85
 
-#1080ti
+
 nvidia-smi -i 0 -pl 190
 nvidia-smi -i 1 -pl 160
-#1080ti
 nvidia-smi -i 2 -pl 190
 nvidia-smi -i 3 -pl 190
-#1070s below
 nvidia-smi -i 4 -pl 160
 nvidia-smi -i 5 -pl 160
 nvidia-smi -i 6 -pl 160
@@ -33,17 +27,17 @@ nvidia-smi -i 11 -pl 160
 
 
 
-nvidia-settings -a [gpu:0]/GPUGraphicsClockOffset[3]=225
-nvidia-settings -a [gpu:1]/GPUGraphicsClockOffset[3]=225
-nvidia-settings -a [gpu:2]/GPUGraphicsClockOffset[3]=225
-nvidia-settings -a [gpu:3]/GPUGraphicsClockOffset[3]=225
+nvidia-settings -a [gpu:0]/GPUGraphicsClockOffset[3]=185
+nvidia-settings -a [gpu:1]/GPUGraphicsClockOffset[3]=185
+nvidia-settings -a [gpu:2]/GPUGraphicsClockOffset[3]=185
+nvidia-settings -a [gpu:3]/GPUGraphicsClockOffset[3]=185
 #POS card keeps overheating no overclock
-nvidia-settings -a [gpu:4]/GPUGraphicsClockOffset[3]=220
-nvidia-settings -a [gpu:5]/GPUGraphicsClockOffset[3]=225
-nvidia-settings -a [gpu:6]/GPUGraphicsClockOffset[3]=225
-nvidia-settings -a [gpu:7]/GPUGraphicsClockOffset[3]=225
-#Projector
-nvidia-settings -a [gpu:8]/GPUGraphicsClockOffset[3]=225
-nvidia-settings -a [gpu:9]/GPUGraphicsClockOffset[3]=225
-nvidia-settings -a [gpu:10]/GPUGraphicsClockOffset[3]=225
-nvidia-settings -a [gpu:11]/GPUGraphicsClockOffset[3]=225
+nvidia-settings -a [gpu:4]/GPUGraphicsClockOffset[3]=125
+nvidia-settings -a [gpu:5]/GPUGraphicsClockOffset[3]=185 
+nvidia-settings -a [gpu:6]/GPUGraphicsClockOffset[3]=185
+nvidia-settings -a [gpu:7]/GPUGraphicsClockOffset[3]=185
+#Crashed once
+nvidia-settings -a [gpu:8]/GPUGraphicsClockOffset[3]=185
+nvidia-settings -a [gpu:9]/GPUGraphicsClockOffset[3]=185
+nvidia-settings -a [gpu:10]/GPUGraphicsClockOffset[3]=185
+nvidia-settings -a [gpu:11]/GPUGraphicsClockOffset[3]=185
